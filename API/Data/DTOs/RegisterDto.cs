f992862d6275403340c6aeea4cdb4dfb0bb20375
DONATAS LASYS <donatukas12@gmail.com>
2021-10-02 21:57:21 +0300

    End of section 7

--- API/Data/DTOs/RegisterDto.cs
+++ API/Data/DTOs/RegisterDto.cs
@@ -10,7 +10,9 @@ namespace API.Data.DTOs
     {
         [Required]
         public string Username { get; set; }
+        
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
